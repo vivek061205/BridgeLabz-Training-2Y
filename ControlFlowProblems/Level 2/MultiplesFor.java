3ced5b82921c38aa07f771d32e781369a8ddaea2
vivek061205 <vivek.in75@gmail.com>
2025-08-31 14:59:18 +0530

    Added MultiplesFor.java - Level 2

---
+++ ControlFlowProblems/Level 2/MultiplesFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MultiplesFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive number less than 100: ");
+        int number = sc.nextInt();
+        for (int i = 100; i >= 1; i--) {
+            if (i % number == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

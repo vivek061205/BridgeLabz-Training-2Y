b9bc5f3be199ac342660e575fdbba0d79f00d000
vivek061205 <vivek.in75@gmail.com>
2025-09-06 13:46:56 +0530

    Added RemoveCharacter

---
+++ String/Extras/RemoveCharacter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RemoveCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        System.out.println("Enter character to remove:");
+        char ch = sc.next().charAt(0);
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ch) result += str.charAt(i);
+        }
+        System.out.println("Modified String: " + result);
+    }
+}

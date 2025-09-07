252f22f1e4dc926a0ec53df4257217049d3cbb1f
vivek061205 <vivek.in75@gmail.com>
2025-09-07 19:39:42 +0530

    Added PrimeChecker

---
+++ BuiltinFunctions/Extras/PrimeChecker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if (isPrime(n))
+            System.out.println(n + " is a prime number.");
+        else
+            System.out.println(n + " is not a prime number.");
+        sc.close();
+    }
+}

9a71af9f2e810f3768af97f618c0f4abf898c345
vivek061205 <vivek.in75@gmail.com>
2025-09-07 19:38:15 +0530

    Added PalindromeChecker

---
+++ BuiltinFunctions/Extras/PalindromeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        if (isPalindrome(str))
+            System.out.println(str + " is a palindrome.");
+        else
+            System.out.println(str + " is not a palindrome.");
+        sc.close();
+    }
+}

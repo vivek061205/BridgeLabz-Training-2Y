feeb1be65cd4d7e8f3f599222833970e4db2db32
vivek061205 <vivek.in75@gmail.com>
2025-09-07 21:03:53 +0530

    Added TemperatureConverter

---
+++ BuiltinFunctions/Extras/TemperatureConverter.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+    // Celsius to Fahrenheit
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    // Fahrenheit to Celsius
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Temperature Converter");
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            System.out.print("Enter temperature in Celsius: ");
+            double celsius = sc.nextDouble();
+            System.out.println("Fahrenheit: " + celsiusToFahrenheit(celsius));
+        } else if (choice == 2) {
+            System.out.print("Enter temperature in Fahrenheit: ");
+            double fahrenheit = sc.nextDouble();
+            System.out.println("Celsius: " + fahrenheitToCelsius(fahrenheit));
+        } else {
+            System.out.println("Invalid choice!");
+        }
+
+        sc.close();
+    }
+}

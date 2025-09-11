0bb0ccd046908c8f5dc32a0976c6fcc66884d30a
vivek061205 <vivek.in75@gmail.com>
2025-09-11 15:38:12 +0530

    Added Student.java in JavaClassAndObject/Level2

---
+++ JavaClassAndObject/Level2/Student.java
@@ -0,0 +1,30 @@
+class Student {
+    String name;
+    int rollNumber;
+    int marks;
+
+    Student(String name, int rollNumber, int marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    String calculateGrade() {
+        if (marks >= 90) return "A";
+        else if (marks >= 75) return "B";
+        else if (marks >= 50) return "C";
+        else return "D";
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student("Priya", 21, 82);
+        s.displayDetails();
+    }
+}

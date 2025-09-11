58b2bd153f7a44dc87a3bc80fd2fda41d56f7869
vivek061205 <vivek.in75@gmail.com>
2025-09-11 16:13:34 +0530

    Added Circle.java in JavaClassAndObject/Level1

---
+++ JavaClassAndObject/Level1/Circle.java
@@ -0,0 +1,26 @@
+class Circle {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    void display() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Circumference: " + getCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle c = new Circle(7);
+        c.display();
+    }
+}

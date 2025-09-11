9eb8a37bcd69c25d4c097ad027a3d534cc47531d
vivek061205 <vivek.in75@gmail.com>
2025-09-11 16:14:05 +0530

    Added MovieTicket.java in JavaClassAndObject/Level2

---
+++ JavaClassAndObject/Level2/MovieTicket.java
@@ -0,0 +1,30 @@
+class MovieTicket {
+    String movieName;
+    int seatNumber;
+    double price;
+
+    MovieTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    void bookTicket(String movie, int seat, double ticketPrice) {
+        this.movieName = movie;
+        this.seatNumber = seat;
+        this.price = ticketPrice;
+        System.out.println("Ticket booked successfully!");
+    }
+
+    void displayTicket() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket("", 0, 0);
+        ticket.bookTicket("Avengers", 15, 250);
+        ticket.displayTicket();
+    }
+}

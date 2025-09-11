bcbaf9a408c3479955cdfe967e959f7f86d94532
vivek061205 <vivek.in75@gmail.com>
2025-09-11 15:37:52 +0530

    Added Employee.java in JavaClassAndObject/Level1

---
+++ JavaClassAndObject/Level1/Employee.java
@@ -0,0 +1,22 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("Rahul", 101, 45000);
+        emp.displayDetails();
+    }
+}

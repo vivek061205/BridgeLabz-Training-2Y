b9c5b2a2021bf1b7053eb9401125c7c89e0a95eb
vivek061205 <vivek.in75@gmail.com>
2025-09-14 12:43:37 +0530

    Added Person.java

---
+++ Constructors/Person.java
@@ -0,0 +1,25 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Vivek", 22);
+        Person p2 = new Person(p1);
+        p1.display();
+        p2.display();
+    }
+}

c2efe9ac91e499f0620e2bd6a0b613e07f2a4c3f
vivek061205 <vivek.in75@gmail.com>
2025-09-14 12:43:44 +0530

    Added LibraryBook.java

---
+++ Constructors/LibraryBook.java
@@ -0,0 +1,33 @@
+class LibraryBook {
+    String title;
+    String author;
+    double price;
+    boolean available;
+
+    LibraryBook(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.available = true;
+    }
+
+    void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println(title + " borrowed successfully!");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+
+    void display() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Price: " + price + ", Available: " + available);
+    }
+
+    public static void main(String[] args) {
+        LibraryBook lb = new LibraryBook("Python", "Guido", 400);
+        lb.display();
+        lb.borrowBook();
+        lb.borrowBook();
+    }
+}

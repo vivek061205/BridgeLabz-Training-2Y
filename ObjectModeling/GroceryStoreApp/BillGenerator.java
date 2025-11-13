0458be117b4769b287a4779985066aade49b0874
vivek061205 <vivek.in75@gmail.com>
2025-11-13 13:42:27 +0530

    Deleted unwanted folders from Functional-interface branch

--- ObjectModeling/GroceryStoreApp/BillGenerator.java
+++
@@ -1,9 +0,0 @@
-public class BillGenerator {
-    public double generateBill(Customer customer) {
-        double total = 0;
-        for (Product product : customer.getProducts()) {
-            total += product.getTotalPrice();
-        }
-        return total;
-    }
-}

8709d5c4786420977d02252af59cda146737c7aa
vivek061205 <vivek.in75@gmail.com>
2025-11-19 10:30:54 +0530

    Add MaxValueKey.java

---
+++ MaxValueKey.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MaxValueKey {
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of("A",10,"B",20,"C",15);
+        String maxKey = null;
+        int max = Integer.MIN_VALUE;
+        for (var e : map.entrySet())
+            if (e.getValue() > max) { max = e.getValue(); maxKey = e.getKey(); }
+        System.out.println("Highest value key = " + maxKey);
+    }
+}
